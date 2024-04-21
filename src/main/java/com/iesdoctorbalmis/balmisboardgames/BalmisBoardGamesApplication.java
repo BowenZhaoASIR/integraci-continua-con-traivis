581433dc22412408189ce6a85472da65f1c9733c
Bowen Zhao <bowzha@alu.edu.gva.es>
2024-04-21 14:18:00 +0200

    added error to trigger error test

--- src/main/java/com/iesdoctorbalmis/balmisboardgames/BalmisBoardGamesApplication.java
+++ src/main/java/com/iesdoctorbalmis/balmisboardgames/BalmisBoardGamesApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BalmisBoardGamesApplication {
 	
-	@GetMapping("/hello")
+	@GetMapping("/helloworld")
 	public String helloworld() {
 		return "hello world";
 	}
